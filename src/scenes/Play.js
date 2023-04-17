af8c1752f440732f00b0cf8e54da3c1a955d849c
Abel Goy <agoy@ucsc.edu>
2023-04-17 16:26:23 -0700

    no issue, just need to choose localhost

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -17,7 +17,7 @@ class Play extends Phaser.Scene {
         // green UI background
         this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0, 0);
         // white borders
-        this.add.rectangle(0, 0, game.config.width + game.config.width, borderUISize * 2, 0xFFFFFF)
+        this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF)
         this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0 ,0);
         this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0 ,0);
         this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0 ,0);
