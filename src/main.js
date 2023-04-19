28b310aeb14c9d137da86b4c22a809e853ff1f85
Abel Goy <agoy@ucsc.edu>
2023-04-18 17:59:04 -0700

    Firing stuff

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+const { Phaser } = require("../lib/phaser");
+
 let config = {
     type: Phaser.CANVAS,
     width: 640,
@@ -10,4 +12,13 @@ let game = new Phaser.Game(config);
 // set UI sizes
 let borderUISize = game.config.height / 15;
 let borderPadding = borderUISize / 3;
+
+// reserve keyboard vars
+let keyF, keyR, ketLEFT, keyRIGHT;
+
+//define keys
+keyF = this.InputDeviceInfo.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
+keyR = this.InputDeviceInfo.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
+keyLEFT = this.InputDeviceInfo.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+keyRIGHT = this.InputDeviceInfo.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
 
