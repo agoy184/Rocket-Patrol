3343ec96a31ccf421630996586e55931a9f11527
Abel Goy <agoy@ucsc.edu>
2023-04-18 18:55:23 -0700

    Ships Flying

--- src/main.js
+++ src/main.js
@@ -13,9 +13,3 @@ let borderPadding = borderUISize / 3;
 
 // reserve keyboard vars
 let keyF, keyR, keyLEFT, keyRIGHT;
-
-//define keys
-keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
-keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
-keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
-keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
