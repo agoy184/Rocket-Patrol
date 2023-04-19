10f7ea0082b5efa8797760b5ed36bd8bf87a0ad8
Abel Goy <agoy@ucsc.edu>
2023-04-18 17:48:13 -0700

    Rocket added

---
+++ src/prefabs/Rocket.js
@@ -0,0 +1,9 @@
+//RocketPrefab
+class Rocket extends Phaser.GameObjects.Sprite {
+    constructor (scene, x, y, texture, frame) {
+        super(scene, x, y, texture, frame);
+
+        //add obj to existing scene
+        scene.add.existing(this);
+    }
+}
